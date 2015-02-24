09f8a7f0d1ef654a8296c9edf3ff40aec2eb81f3
fabio.lacio <fabio.lacio@SCZ-SIST-003.laslomas.com.bo>
2015-02-24 17:39:13 -0400

    Proyecto de entidades , con la entidad Vehiculo creada con dos atributos (id, descripcion)

---
+++ src/com/laslomas/dominio/Vehiculo.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.laslomas.dominio;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author fabio.lacio
+ */
+@Entity
+@Table(name = "vehiculo")
+public class Vehiculo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String descripcion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
